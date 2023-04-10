a3b43eb578c7d27fd18af2cab607746dc5bc91da
Kim Tuấn Kiên <kimtuankien2002@gmail.com>
2023-04-10 22:38:47 +0700

    add prettier lint-staged husky

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,15 +1,14 @@
-
 class NewsController {
-    // [GET] /news
-    index(req, res) {
-        res.render('news');
-    }
+  // [GET] /news
+  index(req, res) {
+    res.render('news');
+  }
 
-    //[GET] /news/:slug
-    show(req, res){
-        res.send('NEWS DETAIL!!!');
-    }
+  //[GET] /news/:slug
+  show(req, res) {
+    res.send('NEWS DETAIL!!!');
+  }
 }
 
-module.exports = new NewsController;
-
+module.exports = new NewsController(); // tạo 1 đối tượng và export ra ngoài
+// export cái gì sẽ nhận được cái đó khi require
