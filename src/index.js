8f789659af47ff7962d3c786f7a26ee9c1da4847
Kim Tuấn Kiên <kimtuankien2002@gmail.com>
2023-04-10 14:35:10 +0700

    creat routes, app/controllers folders

--- src/index.js
+++ src/index.js
@@ -5,8 +5,14 @@ const { engine } = require ('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname,'public')));
 
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
 // http logger (tiêu chuẩn)
 app.use(morgan('combined'));
 // template engine
@@ -16,14 +22,9 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-// console.log('PATH: ', path.join(__dirname, 'resources/views'))
-app.get('/', (req, res) => {
-    res.render('home')
-})
+// Routes init
+route(app);
 
-app.get('/news', (req, res) => {
-  res.render('news')
-})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
