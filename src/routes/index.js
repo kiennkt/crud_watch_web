b067cdaba3a4608642fadce70417855c748b2423
Kim Tuấn Kiên <kimtuankien2002@gmail.com>
2023-04-10 14:42:25 +0700

    creat app/controllers routes folders

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,25 +3,10 @@ const siteRouter = require('./site');
 
 
 function route(app){
-    // console.log('PATH: ', path.join(__dirname, 'resources/views'))
-    // app.get('/', (req, res) => {
-    //     res.render('home');
-    // });
 
-    // app.get('/news', (req, res) => {
-    //     res.render('news');
-    // });
     app.use('/news', newsRouter);
     
     app.use('/', siteRouter);
-    // app.get('/search', (req, res) => {
-    //     res.render('search');
-    // });
-    
-    // app.post('/search', (req, res) => {
-    //     // console.log(req.body)
-    //     res.render('search');
-    // });
 }
 
 module.exports = route;
